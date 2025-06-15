e9c338e000090d9d9c7414ec664559e345a5f67a
ADITYAAV80 <adityaav80@gmail.com>
2025-06-15 19:32:27 +0530

    Induvidual Job pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage'
+import JobPage, { jobLoader } from './pages/JobPage'
 import NotFoundPage from './pages/NotFoundPage'
 import MainLayout from './layouts/MainLayout'
 
@@ -10,6 +11,7 @@ const router = createBrowserRouter(
     <Route path='/' element= {<MainLayout/>}>
       <Route index element = {<HomePage/>} />
       <Route path='/jobs' element = {<JobsPage/>} />
+      <Route path='/jobs/:id' element={<JobPage/>} loader = {jobLoader}/>
       <Route path='*' element = {<NotFoundPage/>} />
     </Route>
   )
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { FaMapMarker} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const JobListing = ({job}) => {
 
@@ -39,7 +39,7 @@ const JobListing = ({job}) => {
                     {job.location}
                 </div>
                 <Link
-                    to={`job/${job.id}`}
+                    to={`/jobs/${job.id}`}
                     className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                 Read More
